44e7d8d0e7870c8b11fdf1c078e45092fbfb91c3
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2025-06-21 15:57:43 +0800

    basic serverFN created

--- src/routes/algorithms/_layout/sorting.tsx
+++ src/routes/algorithms/_layout/sorting.tsx
@@ -3,20 +3,33 @@ import { createServerFn, useServerFn } from "@tanstack/react-start";
 import { useQuery } from "@tanstack/react-query";
 import SideBar from "~/components/algorithms/SideBar";
 import TopControls from "~/components/algorithms/TopControls";
+import { z } from "zod";
+import { useState } from "react";
 
 export const Route = createFileRoute("/algorithms/_layout/sorting")({
   component: RouteComponent,
 });
 
-const getServerTime = createServerFn().handler(async ({ data }) => {
-  return new Date().toISOString();
-});
+const generateRandomNumbers = createServerFn()
+  .validator(z.object({ count: z.number().min(1).max(999999) }))
+  .handler(async ({ data }): Promise<number[]> => {
+    const count = data.count;
+    const numbers = [];
+    for (let i = 0; i < count + 1; i++) {
+      numbers.push(Math.floor(Math.random() * 10000));
+    }
+    return numbers;
+  });
 
 function RouteComponent() {
-  const getServerTimeFn = useServerFn(getServerTime);
+  const [count, setCount] = useState(100);
+
+  const generateRandomNumbersFn = useServerFn(generateRandomNumbers);
   const { data } = useQuery({
-    queryKey: ["getServerTime", "sorting"],
-    queryFn: getServerTimeFn,
+    queryKey: ["generateRandomNumbers", "sorting", count],
+    queryFn: () => generateRandomNumbersFn({ data: { count } }),
+    staleTime: 1000 * 60 * 60 * 24,
+    gcTime: 1000 * 60 * 60 * 24,
   });
 
   return (
@@ -24,7 +37,9 @@ function RouteComponent() {
       <SideBar title="Sorting" />
       <div className="flex flex-1 flex-col">
         <TopControls />
-        <div className="flex flex-1 flex-col gap-4 bg-blue-500 p-4">{data}</div>
+        <div className="flex flex-1 flex-col gap-4 bg-blue-500 p-4">
+          {data?.map((number, i) => <div key={i}>{`${i}\) ${number}`}</div>)}
+        </div>
       </div>
     </div>
   );
